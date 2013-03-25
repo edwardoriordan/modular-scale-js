1924ed4b3ceff331df1173fcfe9a3db22527479e
Edward O'Riordan <e.h.oriordan@gmail.com>
2013-03-25 15:28:42 +0000

    init commit

---
+++ ui.js
@@ -0,0 +1,77 @@
+jQuery(document).ready(function($) {
+  
+  var modular = new ModularScale({
+    baseSize: 18,
+    debug: true,
+    importantNumber: 150,
+    ratio: 'golden'
+  });
+
+  var applyFontSize = function() {
+    $('.ms0').css('font-size', modular.ms(0));
+    $('.ms1').css('font-size', modular.ms(1));
+    $('.ms2').css('font-size', modular.ms(2));  
+    $('.ms3').css('font-size', modular.ms(3));
+    $('.ms4').css('font-size', modular.ms(4));  
+    $('.ms5').css('font-size', modular.ms(5));
+    $('.ms6').css('font-size', modular.ms(6));
+    $('.ms7').css('font-size', modular.ms(7));  
+    $('.ms8').css('font-size', modular.ms(8));
+    $('.ms9').css('font-size', modular.ms(9));
+    $('.intro').css('font-size', modular.ms(-1));
+  }
+
+  var setUiEventHandler = function() {
+    $('#special-number').change(function(){
+      modular.set('importantNumber', $(this).val() * 1);
+      applyFontSize();
+    });
+
+    $('#ratio').change(function(){
+      modular.set('ratio', modular.ratios[$(this).val()]);
+      applyFontSize();
+    });
+
+    $('#font-size').change(function(){
+      modular.set('baseSize', $(this).val() * 1);
+      applyFontSize();
+    });
+
+    $('.class-range').change(function() {
+      $(this).parent().removeClass().addClass('ms' + $(this).val());
+      applyFontSize();
+    });
+  }
+
+  var init = function() {
+    $('#special-number').val(modular.get('importantNumber'));
+    $('#font-size').val(modular.get('baseSize'));
+    $('#ratio').val('golden');
+    
+    $('#font-size').change();
+    showHideNumVales();
+  }
+
+  var showHideNumVales = function() {
+    $('body').keydown(function(event) {
+      if (event.keyCode === 83) {
+        $('.ui').toggle();
+      }
+    });
+  }
+
+  var headingScales = function() {
+    $('.class-range').hide();
+    $('h1, h2, h3, h4, h5, h6').hover(function() {
+      $(this).find('.class-range').show();
+    }, function() {
+      $(this).find('.class-range').hide();
+    });
+  }
+
+  headingScales();
+  setUiEventHandler();
+  init();
+});
+
+//console.log( msNeg(1) );
